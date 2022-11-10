5ee6e2008e022c4b2852cefcc4406d471838260d
CamillePerier <cperier28@gmail.com>
2022-11-10 15:18:55 +0100

    Bonjour

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+class index
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
