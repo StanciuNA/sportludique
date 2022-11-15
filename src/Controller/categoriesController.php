58a5a6d450dc7dcc9a29226ed14ea60f6cb39c41
Sportludique <sportludique3@outlook.fr>
2022-11-15 16:07:47 +0100

    added basic templates

--- src/Controller/categoriesController.php
+++ src/Controller/categoriesController.php
@@ -5,10 +5,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class CategoriesController extends AbstractController
 {
-    public function index(): Response
+    public function category(): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('category.html.twig');
     }
 }
