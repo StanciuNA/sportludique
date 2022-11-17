6916143841a412e8922344aff0aeffd80e302bad
CamillePerier <cperier28@gmail.com>
2022-11-17 14:30:09 +0100

    routes

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+}
