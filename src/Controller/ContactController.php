d146525e8a31937ba630aad22b92b364b9cb4cd1
CamillePerier <cperier28@gmail.com>
2022-11-17 15:20:19 +0100

    rouites

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/contact')]
 class ContactController extends AbstractController
 {
     public function contact(): Response
