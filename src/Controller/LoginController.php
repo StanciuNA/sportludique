692fe1b405b6fc67ea5368c34361376fb19cfc0b
Daisy0402 <tacitadaisy@gmail.com>
2023-03-07 18:26:43 +0100

    Système d'authentification fonctionel

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,8 +18,16 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('login/index.html.twig', [
+            
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
     }
+
+    #[Route("/logout", name:"app_logout", methods:"GET" )]
+   public function logout()
+   {
+       // controller can be blank: it will never be executed!
+       throw new \Exception('Don\'t forget to activate logout in security.yaml');
+   }
 }
